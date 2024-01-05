517ef46abc17480136b2c4893661f2018f4080d8
Jo Youjin <youjinjoy@gmail.com>
2024-01-05 23:17:11 +0900

    project restart

---
+++ src/main/java/com/assignment/jungle/crudjwtproject/CrudJwtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.jungle.crudjwtproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudJwtProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudJwtProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/jungle/crudjwtproject/CrudJwtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.jungle.crudjwtproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJwtProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
